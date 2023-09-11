2cdd6fb0aa3b22d93bb39fc36f87e60b87257787
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-11 17:42:37 +0900

    10172 개

--- BaekJoon/BaekJoon/MySolution.cpp
+++ BaekJoon/BaekJoon/MySolution.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int main() {
-	std::cout << "\\    /\\" <<	std::endl;
-	std::cout << " )  ( ')" << std::endl;
-	std::cout << "(  /  )" << std::endl;
-	std::cout << " \\(__)|" << std::endl;
+	std::cout << "|\\_/|" <<	std::endl;
+	std::cout << "|q p|   /}" << std::endl;
+	std::cout << "( 0 )\"\"\"\\" << std::endl;
+	std::cout << "|\"^\"\`    |" << std::endl;
+	std::cout << "||_/=\\\\__|" << std::endl;
 
 
 }
