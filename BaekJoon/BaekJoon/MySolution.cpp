c1b4a3cfec9a32620f98aeaa1a91c4721c73bd9b
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-11 17:47:49 +0900

    반복 long int 25314

--- BaekJoon/BaekJoon/MySolution.cpp
+++ BaekJoon/BaekJoon/MySolution.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-	std::cout << "|\\_/|" <<	std::endl;
-	std::cout << "|q p|   /}" << std::endl;
-	std::cout << "( 0 )\"\"\"\\" << std::endl;
-	std::cout << "|\"^\"\`    |" << std::endl;
-	std::cout << "||_/=\\\\__|" << std::endl;
-
-
+	int N = 0;
+	std::cin >> N;
+	N = N / 4;
+	for (int i = 0; i < N; i++) {
+		std::cout << "long ";
+	}
+	std::cout << "int";
 }
