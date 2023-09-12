b7e75b60b7dd24010e7c9f653a5eeb69a907c6fe
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-12 10:13:32 +0900

    만들고보니 이중배열 필요없을듯

--- BaekJoon/BaekJoon/MySolution.cpp
+++ BaekJoon/BaekJoon/MySolution.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 
 int main() {
-	int N = 0;
-	std::cin >> N;
-	N = N / 4;
-	for (int i = 0; i < N; i++) {
-		std::cout << "long ";
+
+	//int* basket[N];
+	//바구니 == 배열
+
+
+	int N, M = 0;
+	int i, j, k, tryout = 0;
+	//1 ≤ i ≤ j ≤ N, 1 ≤ k ≤ N)
+	std::cin >> N >> M;
+	char** basket = new char*[N];
+	for (int p = 0; p < N; p++) {
+		basket[p] = new char[N];
 	}
-	std::cout << "int";
+	for (int p = 0; p < N; p++) {
+		for (int r = 0; r < N; r++) {
+			basket[p][r] = 0;
+		}
+	}
+	for (; tryout < M; tryout++) {
+		std::cin >> i >> j >> k;
+		if (i > N || j > N || k > N) {
+			std::cout << "range error" << std::endl;
+		}
+		else {
+			// buffer = i j k
+		}
+
+	}
 }
