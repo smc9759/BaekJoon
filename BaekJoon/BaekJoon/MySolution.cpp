1c58fa8c94a95f438706e9e8dd07a9e9dbd706a8
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-12 11:11:21 +0900

    배ㅔ열 크기가 1이길래 봤더니 할당을 선언에서 하고있어서 안됨

--- BaekJoon/BaekJoon/MySolution.cpp
+++ BaekJoon/BaekJoon/MySolution.cpp
@@ -5,7 +5,7 @@ class Basket {
 	int i, j, k, tryout = 0;
 
 public:
-	int* basket = new int[N];
+	int* basket;;
 	void CreateBasket();
 	void PutTheBall(Basket& _basket);
 	int BallNumInside(int num) const { return basket[num]; };
@@ -14,6 +14,7 @@ public:
 
 void Basket::CreateBasket() {
 	std::cin >> N >> M;
+	basket = new int[N];
 	for (int p = 0; p < N; p++) {
 		basket[p] = 0;
 	}
@@ -28,12 +29,12 @@ void Basket::PutTheBall(Basket& _basket) {
 		else {
 			// buffer = i j k
 			//is ball in?
-			for (; i < j; i++) {
+			for (; i <= j; i++) {
 				//if (!_basket.BallNumInside[i]) {
 				//	//공이 들어있으면
 
 				//}
-				_basket.basket[i] = k;
+				_basket.basket[i -1] = k;
 			}
 		}
 	}
