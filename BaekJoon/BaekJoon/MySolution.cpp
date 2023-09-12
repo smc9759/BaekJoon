4a14b65cac2343e98524623542df6a31aa502e40
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-12 10:27:41 +0900

    CLASS NAME CAPITAL

--- BaekJoon/BaekJoon/MySolution.cpp
+++ BaekJoon/BaekJoon/MySolution.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 
-class basket {
-public:
+class Basket {
 	int N, M = 0;
 	int i, j, k, tryout = 0;
+
+public:
 	int* basket = new int[N];
 	void CreateBasket();
-	
+	void PutTheBall();
+	int BallNumInside(const Basket& _basket);
 };
 
-void basket::CreateBasket() {
+void Basket::CreateBasket() {
 	std::cin >> N >> M;
 	for (int p = 0; p < N; p++) {
 		basket[p] = 0;
 	}
 }
 
-int main() {
-
-	basket Mybasket;
-
-	Mybasket.CreateBasket();
-
-
-
+void Basket::PutTheBall() {
 	for (; tryout < M; tryout++) {
 		std::cin >> i >> j >> k;
-		if (i > N || j > N || k > N|| i>j) {
+		if (i > N || j > N || k > N || i>j) {
 			std::cout << "range error" << std::endl;
 		}
 		else {
@@ -37,6 +32,19 @@ int main() {
 	}
 }
 
-int BallNumberInside const(int* _basket) {
+int BallNumInside(const Basket& _basket) {
+
+}
+int main() {
+
+	Basket Mybasket;
+
+	Mybasket.CreateBasket();
+
+
+
+
+}
+
 
 }
